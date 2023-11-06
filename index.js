a03e0efb4d418d0e168af4cbd4c1a798ec459145
Rajeeradha <127470577+Rajeeradha@users.noreply.github.com>
2023-11-07 00:43:17 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 //require("dotenv").config();
 const express = require("express");
+const bodyParser = require('body-parser');
 
 const app = express();
-app.use(express.json());
+app.use(bodyParser.json());
 
 const rooms = [
   {
